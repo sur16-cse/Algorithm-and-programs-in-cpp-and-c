82452bab107f6acbe4cdea16bbb2f8d510be1b7a
surbhi kumari <surbhikumaridav@gmail.com>
2022-07-03 22:27:48 +0530

    pattern more questions

--- Pattern/Half_pyramid_using_numbers.cpp
+++ Pattern/Half_pyramid_using_numbers.cpp
@@ -1,9 +1,9 @@
 /*
-    * * * * * 
-    * * * *
-    * * *
-    * *
-    *
+    1
+    22
+    333
+    4444
+    55555
 */
 
 #include<bits/stdc++.h>
@@ -12,9 +12,9 @@ using namespace std;
 int main(){
     int n;
     cin>>n;
-    for(int i=n;i>=1;i--){
+    for(int i=1;i<=n;i++){
         for(int j=1;j<=i;j++){
-            cout<<"*"<<" ";
+           cout<<i;
         }
         cout<<endl;
     }
